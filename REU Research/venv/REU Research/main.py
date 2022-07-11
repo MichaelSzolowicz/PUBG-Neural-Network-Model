fcf51faed2ba14a5a70d47c8a0327e87fa0192e5
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2022-07-11 11:34:40 -0700

    Multi-match data prep
    main.py can now prepare data from multiple matches at once. Furthermore, you can now extract data to a csv using chicken_dinner without turning the data into tensors. Tensors can either be created optionally or after the fact.

--- REU Research/venv/REU Research/main.py
+++ REU Research/venv/REU Research/main.py
@@ -6,45 +6,69 @@ import pubg_plots
 import pubg_tensors
 import preparedata
 import csv
+import torch
+
+
+def import_match_data(id_list, positions_csv_list):
+    api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1NWQ4NWU1MC1jODE3LTAxM2EtYzc4Yi03OWE4NjMxM2U1MTMiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjU0NTU1MTE5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1YmctZGF0YS1wcm9jIn0.K4JaJpbDJpnkt2zPfbt9L3OASg94B7bCgGrliksa_tI"
+    pubg = PUBG(api_key, "pc-na")
+
+    for count, id in enumerate(id_list):
+        # This line of code is used if you have the match id of a certain match.
+        match = pubg.match(id)
+
+        # This block of code creates a csv file for player positions given the match_id. User must manually enter file
+        # location for csv in second parameter.
+        telemetry = match.get_telemetry()
+        pubg_csv.player_position_csv(telemetry, positions_csv_list[count])
+        pubg_csv.fill_csv(positions_csv_list[count])
+
+
+def prepare_training_data(positions_csv_list, packets_list, training):
+    tensor_x = torch.FloatTensor()
+    tensor_y = torch.FloatTensor()
+    timestamps_list = []
+
+    for i in range(len(positions_csv_list)):
+       # for pos, pack in zip()
+        x, y, timestamps = pubg_tensors.get_tensors(positions_csv_list[i], packets_list[i])
+        tensor_x = torch.cat((tensor_x, x))
+        tensor_y = torch.cat((tensor_y, y))
+        timestamps_list.extend(timestamps)
+
+    # This block of code either prepares the data for training the model or for evaluating the predicted coordinates
+    if training:
+        timestamps_list = preparedata.prepare_data_training(tensor_x, tensor_y, timestamps_list)
+    else:
+        preparedata.prepare_data_valuation(x, y)
+
+    # This block of code creates a csv file for the timestamps
+    csv_writer = csv.writer(open('tensor_timestamps.csv', 'w', newline=''))
+    for timestamp in timestamps_list:
+        csv_writer.writerow([timestamp])
 
 
 # Main function
 def main():
-    api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1NWQ4NWU1MC1jODE3LTAxM2EtYzc4Yi03OWE4NjMxM2U1MTMiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjU0NTU1MTE5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1YmctZGF0YS1wcm9jIn0.K4JaJpbDJpnkt2zPfbt9L3OASg94B7bCgGrliksa_tI"
-    match_id = '7139785b-5b4d-46a8-ae39-32563083d4c2'
-    player_positions = 'PlayerPositions/player_pos_6_28_22_1358.csv'
-    packets = 'NetworkPackets/packets_6_28_22_1358.json'
-    pubg = PUBG(api_key, "pc-na")
+    id_list = ['490b5eae-0667-452b-aade-32bc6ed54742', 'bbba3d1c-3d9e-457e-9c3d-59f80b330e3c']
+    player_positions_list = ['PlayerPositions/player_pos_6_29_22_1607.csv', 'PlayerPositions/player_pos_6_29_22_1626.csv']
+    packets_list = ['NetworkPackets/packets_6_29_22_1607.json', 'NetworkPackets/packets_6_29_22_1626.json']
 
-    # This block of code gets players as well as current season, used to get match id of latest game played by player
+    inp = input('Import match data? y / n: ')
+    match inp:
+        case 'y':
+            import_match_data(id_list, player_positions_list)
+        case _:
+            pass
 
-    players = pubg.players_from_names(["Mszolo"])
-    Mszolo = players[0]
-    current_season = pubg.current_season()
-    # match_id = pubg_info.get_season_duo_match_ids(Mszolo, current_season)
-
-    # This line of code is used if you have the match id of a certain match.
-    match_id = pubg.match(match_id)
-
-    # This block of code creates a csv file for player positions given the match_id. User must manually enter file
-    # location for csv in second parameter.
-    telemetry = match_id.get_telemetry()
-    pubg_csv.player_position_csv(telemetry, player_positions)
-    pubg_csv.fill_csv(player_positions)
-
-    # This line of code creates tensors for player positions: x for byte data and y for player positions. It also
-    # returns a list of timestamps that match the packet received and player positions at that timestamp
-    x, y, timestamps = pubg_tensors.get_tensors(player_positions, packets)
-
-    # This block of code either prepares the data for training the model or for evaluating the predicted coordinates
-    # User most comment one out for whatever they are trying to do.
-    # timestamps = preparedata.prepare_data_training(x, y, timestamps)
-    preparedata.prepare_data_valuation(x, y)
-
-    # This block of code creates a csv file for the timestamps
-    csv_writer = csv.writer(open('tensor_timestamps.csv', 'w', newline=''))
-    for timestamp in timestamps:
-        csv_writer.writerow([timestamp])
+    inp = input('Prepare data? training / valuation / none: ')
+    match inp:
+        case 'training':
+            prepare_training_data(player_positions_list, packets_list, True)
+        case 'valuation':
+            prepare_training_data(player_positions_list, packets_list, False)
+        case _:
+            pass
 
 
 if __name__ == "__main__":
